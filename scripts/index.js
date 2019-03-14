9ba4653568366e0ec98f5c8916c42e45e087e9b1
Eric Mauldin <emauldin84@gmail.com>
2019-03-14 09:04:53 -0400

    adds json folder with atlWeather.json and atlMovies.json

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const weatherForecastUrl = `http://api.openweathermap.org/data/2.5/forecast?zip=30312,us&units=imperial&appid=58aa95e71dfc9f2a24bed5cd37d8213e`
+
+console.log(weatherForecastUrl)
+
+fetch(weatherForecastUrl)
+.then (function(response) {
+    return response.json();
+})
+.then (function(weatherData) {
+    console.log(weatherData)
+})
+    
