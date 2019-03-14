249e95100063263436efb8828441d75a827a072c
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-14 11:07:21 -0400

    adds first rainy instance to page

--- scripts/weather.js
+++ scripts/weather.js
@@ -20,14 +20,14 @@ function getWeatherData(zipcode) {
         // take the second promise and console log the data
         .then(function(weatherObject) {
             createCityInfo(weatherObject);
-            firstTimeRain(weatherObject);
+            allRainyDays(weatherObject);
         })
 }
 
 // this function takes the weather object from the fetch chain, and starts displaying info to the user
 function createCityInfo(weatherObject) {
     // creates pointer to weather div
-    const weatherDiv = document.querySelector('[data-weather]');
+    weatherDiv = document.querySelector('[data-weather]');
     // creates a h2 element for city info
     let cityH2 = document.createElement('h2');
     // assigns text content
@@ -39,7 +39,7 @@ function createCityInfo(weatherObject) {
 }
 
 // this will find the first instance of rain in the 5 day forecast
-function firstTimeRain(weatherObject) {
+function allRainyDays(weatherObject) {
     // array of data for each 3 hour period for 5 days
     let daysArray = weatherObject.list
     // initialize empty array of rainy days
@@ -52,5 +52,18 @@ function firstTimeRain(weatherObject) {
             rainyDays.push(day.dt_txt);
         }
     })
-    console.log(rainyDays);
+    firstRainyDay(rainyDays);
+}
+
+// this function will be passed the array of rainy days as an argument, it will take the first element in the array, and print it to the page
+
+function firstRainyDay(rainyDaysArray) {
+    // save first rainy instance in array
+    let firstRainDay = rainyDaysArray[0];
+    let rainyH2 = document.createElement('h2');
+    rainyH2.textContent = `Your first rainy day is: ${firstRainDay}`;
+
+    weatherDiv.append(rainyH2);
+
+
 }
