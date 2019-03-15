c1cc155fb7d6af74adfe40988bb27850351596d0
Eric Mauldin <emauldin84@gmail.com>
2019-03-14 21:37:43 -0400

    adds ability to press 'enter' key to submit zipcode field to zipCodeVar.js file

--- scripts/zipCodeVar.js
+++ scripts/zipCodeVar.js
@@ -1,11 +1,18 @@
 
 // pointer to submit button
 function lookForSubmitClick() {
-
+    // Triggers the zip code submit on submit button click
     let submitButton = document.querySelector('[data-zipcode]');
     submitButton.addEventListener('click', getZipCode)
-}
 
+    // Triggers the zip code submit on pressing enter
+    let zipField = document.querySelector('input');
+    zipField.addEventListener('keypress', function(event){
+        if(event.keyCode === 13) {
+        getZipCode();
+        }
+    })
+}
 
 
 function getZipCode() {
